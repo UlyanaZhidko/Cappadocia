0da43365e56c75172cede7ab97a66e1ab7c70a28
UlyanaZhidko <Zhidko.Ulyana@gmail.com>
2020-06-04 15:42:44 +0300

    2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$(function(){
+$(function () {
+    $('.fairy-tail-slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="/images/left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="/images/right.svg" alt=""></button>',
+        autoplay: 2500
+    });
 
+    $('.our-trip-slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="/images/left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="/images/right.svg" alt=""></button>',
+        autoplay: 2500
+    });
 });
