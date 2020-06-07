410426deefd67b10fa3278394fc51a2cad175ba9
UlyanaZhidko <Zhidko.Ulyana@gmail.com>
2020-06-07 19:50:44 +0300

    07.06

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 $(function () {
-    $('.fairy-tail-slider').slick({
+    $('.fairy-tail-slider, .our-trip-slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="/images/left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="/images/right.svg" alt=""></button>',
-        autoplay: 2500
-    });
-
-    $('.our-trip-slider').slick({
-        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="/images/left.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="/images/right.svg" alt=""></button>',
-        autoplay: 2500
+        autoplay: 2500,
+        responsive: [
+            {
+                breakpoint: 601,
+                settings: {
+                    arrows: false
+                }
+            },
+        ]
     });
 });
